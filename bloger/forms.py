ef40fa58012a7f75ddaca955cae30492f2538f9a
E-digital1000 <techelvis7@gmail.com>
2023-06-01 15:36:43 +0000

    level

--- bloger/forms.py
+++ bloger/forms.py
@@ -21,4 +21,4 @@ class UserProfileForm(forms.ModelForm):
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog_Post
-        fields = ['image', 'title', 'body', 'slug']
+        fields = ['title', 'body', 'slug']
