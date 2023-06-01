b8f659f31d4c8220b4a4d9505fbdf48f226cc6ec
E-digital1000 <techelvis7@gmail.com>
2023-06-01 09:59:26 +0000

    y

--- projectname/settings.py
+++ projectname/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = ['ebog.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
+    'cloudinary',
     'bloger',
     
 ]
