e7a1e41a066eca2883da664963c10c0b358b47e0
E-digital1000 <techelvis7@gmail.com>
2023-06-01 09:56:08 +0000

    false

--- projectname/settings.py
+++ projectname/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$v79hsag()wa5o#x#^yahn2exx)#18cd7q!f^4^647(^k4jgca'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ebog.vercel.app']
 
